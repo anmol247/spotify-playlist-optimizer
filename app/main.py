097429dc8a196b6379ea051c917e7a3eec0d30a8
anmol247 <anmoltiwari11@gmail.com>
2026-05-25 11:45:16 +0530

    Add playlist track extraction endpoint

--- app/main.py
+++ app/main.py
@@ -8,6 +8,19 @@ from dotenv import load_dotenv
 load_dotenv()
 app = FastAPI()
 
+spotify_client = None
+
+# def get_spotify_client():
+
+#     return spotipy.Spotify(
+#         auth_manager=SpotifyOAuth(
+#             client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+#             client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+#             redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+#             scope="playlist-read-private"
+#         )
+#     )
+
 sp_oauth = SpotifyOAuth(
     client_id = os.getenv("SPOTIPY_CLIENT_ID"),
     client_secret = os.getenv("SPOTIPY_CLIENT_SECRET"),
@@ -23,21 +36,39 @@ def home():
 @app.get("/callback")
 def callback(code: str):
 
+    global spotify_client
+
     token_info = sp_oauth.get_access_token(code)
 
-    sp = spotipy.Spotify(auth=token_info["access_token"])
+    spotify_client = spotipy.Spotify(
+        auth=token_info["access_token"]
+    )
 
-    playlists_response = sp.current_user_playlists()
+    return {"message": "Login successful"}
+
+@app.get("/playlist/{playlist_id}")
+def get_playlist(playlist_id: str):
+
+    playlist_data = spotify_client.playlist_items(playlist_id)
 
     results = []
 
-    for playlist in playlists_response["items"]:
+    for item in playlist_data["items"]:
+
+        track = item.get("item")
+
+        if not track:
+            continue
 
         results.append({
-            "id": playlist.get("id"),
-            "name": playlist.get("name"),
-            "tracks": playlist.get("items", {}).get("total")
+            "track_name": track.get("name"),
+            "artist": track["artists"][0]["name"],
+            "album": track["album"]["name"],
+            "track_id": track.get("id")
         })
 
     return results
+
+
+    # return results
     
